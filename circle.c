e9fdac8f6bb9e34d7e987be8428af77bdc33a5c1
Naseem Stanikzy <stanikzy.naseem@gmail.com>
2022-04-21 13:17:47 -0700

    Create circle.c

---
+++ circle.c
@@ -0,0 +1,8 @@
+struct Circle {
+
+	double radius;		  // Storage space Radius 
+	double diameter;	  // Storage space for Diameter 
+	double area;		  // Storage space for Area 
+	double circumference; // Storage space for Circumference 
+
+}circle, circle_object;
